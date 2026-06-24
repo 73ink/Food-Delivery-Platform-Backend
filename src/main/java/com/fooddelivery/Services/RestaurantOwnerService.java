501033d324d6929b68c1ae9992c83769bfe7af62
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-24 10:37:38 +0400

    Create a new restaurant owner, if statement to prevent dublicated owner by email

--- src/main/java/com/fooddelivery/Services/RestaurantOwnerService.java
+++ src/main/java/com/fooddelivery/Services/RestaurantOwnerService.java
@@ -1,12 +1,39 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.DTO.request.RestaurantOwnerRequestDTO;
+import com.fooddelivery.DTO.response.RestaurantOwnerResponseDTO;
+import com.fooddelivery.Entities.RestaurantOwner;
+import com.fooddelivery.Exceptions.DuplicateResourceException;
+import com.fooddelivery.Exceptions.ResourceNotFoundException;
+import com.fooddelivery.Repositories.RestaurantOwnerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 // RestaurantOwnerService contains business logic related to restaurant owners.
 @Service
 @RequiredArgsConstructor
 public class RestaurantOwnerService {
+
+    private final RestaurantOwnerRepository restaurantOwnerRepository;
+
+    // Create a new restaurant owner.
+    @Transactional
+    public RestaurantOwnerResponseDTO createRestaurantOwner(RestaurantOwnerRequestDTO dto) {
+
+        // Prevent duplicate owner email.
+        if (restaurantOwnerRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new DuplicateResourceException("Restaurant owner email already exists");
+        }
+
+        RestaurantOwner owner = dto.toEntity();
+
+        RestaurantOwner savedOwner = restaurantOwnerRepository.save(owner);
+
+        return RestaurantOwnerResponseDTO.fromEntity(savedOwner);
+    }
 
 
 }
