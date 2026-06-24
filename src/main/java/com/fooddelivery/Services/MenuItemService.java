ac2c7880eec936782a1ae722a62061eeeef96890
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-24 14:09:02 +0400

    MenuItemService contains business logic related to menu items

--- src/main/java/com/fooddelivery/Services/MenuItemService.java
+++ src/main/java/com/fooddelivery/Services/MenuItemService.java
@@ -1,4 +1,18 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Repositories.MenuItemRepository;
+import com.fooddelivery.Repositories.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+// MenuItemService contains business logic related to menu items.
+@Service
+@RequiredArgsConstructor
 public class MenuItemService {
+
+    private final MenuItemRepository menuItemRepository;
+    private final RestaurantRepository restaurantRepository;
+
+
 }
