6919d4f17f40acc76e5cff2139832342b1701e53
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-24 10:21:21 +0400

    Create customer without initial address

--- src/main/java/com/fooddelivery/Services/CustomerService.java
+++ src/main/java/com/fooddelivery/Services/CustomerService.java
@@ -1,13 +1,22 @@
 package com.fooddelivery.Services;
 
-
+import com.fooddelivery.DTO.request.CustomerAddressRequestDTO;
+import com.fooddelivery.DTO.request.CustomerPatchDTO;
+import com.fooddelivery.DTO.request.CustomerRequestDTO;
+import com.fooddelivery.DTO.response.CustomerAddressResponseDTO;
+import com.fooddelivery.DTO.response.CustomerResponseDTO;
+import com.fooddelivery.DTO.response.OrderResponseDTO;
+import com.fooddelivery.Entities.Customer;
+import com.fooddelivery.Entities.CustomerAddress;
+import com.fooddelivery.Exceptions.DuplicateResourceException;
+import com.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.fooddelivery.Repositories.CustomerAddressRepository;
 import com.fooddelivery.Repositories.CustomerRepository;
 import com.fooddelivery.Repositories.OrderRepository;
 import com.fooddelivery.Utils.HelperUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +28,25 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final CustomerAddressRepository customerAddressRepository;
     private final OrderRepository orderRepository;
+
+    // Create customer without initial address.
+    @Transactional
+    public CustomerResponseDTO createCustomer(CustomerRequestDTO dto) {
+
+        // Check if email already exists for an active customer.
+        if (customerRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new DuplicateResourceException("Customer email already exists");
+        }
+
+        Customer customer = dto.toEntity();
+
+        // Generate customer code using overloaded HelperUtils method.
+        customer.setCustomerCode(HelperUtils.generateCode("CUST"));
+
+        Customer savedCustomer = customerRepository.save(customer);
+
+        return CustomerResponseDTO.fromEntity(savedCustomer);
+    }
 
 
 }
