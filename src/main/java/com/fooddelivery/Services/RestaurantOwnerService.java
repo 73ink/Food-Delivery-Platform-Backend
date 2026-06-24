685885deb050bd876cab47a53c780e19710eb2c2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-24 10:36:12 +0400

    RestaurantOwnerService contains business logic related to restaurant owners

---
+++ src/main/java/com/fooddelivery/Services/RestaurantOwnerService.java
@@ -0,0 +1,12 @@
+package com.fooddelivery.Services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// RestaurantOwnerService contains business logic related to restaurant owners.
+@Service
+@RequiredArgsConstructor
+public class RestaurantOwnerService {
+
+
+}
